672ae4d770d9e759f82fdcbb9654b12163752b29
brenne_t <brenne_t@8a830dc2-f48c-4e07-9f9b-d65c713ff458>
2011-11-25 18:07:38 +0000

    implementation of identification
    git-svn-id: https://rendu-svn.etna-alternance.net/TCM-RUBY/001/2013_etna2_TCM-RUBY_1_1/VolcanoFTP/brenne_t@82781 8a830dc2-f48c-4e07-9f9b-d65c713ff458

--- fuji_ftp_client.rb
+++ fuji_ftp_client.rb
@@ -4,6 +4,8 @@ class FujiFtpClient
   def initialize(socket, yml)
     @cs = socket
     @yml = yml
+    @connected = false
+    @name = nil
   end
 
   def ftp_syst(c, args)
@@ -22,9 +24,19 @@ class FujiFtpClient
     0
   end
 
+  def ftp_530(c, args)
+    @cs.write "530 Not logged in.\r\n"
+    0
+  end
+
   def ftp_500(c, args)
     puts "'#{c}' command unrecognized"
     @cs.write "500 '#{c}' command unrecognized\r\n"
+    0
+  end
+
+  def ftp_503(c, args)
+    @cs.write "503 Bad sequence of commands.\r\n"
     0
   end
 
@@ -34,26 +46,75 @@ class FujiFtpClient
   end
 
   def ftp_user(c, args)
-    ftp_502 c, args
+    @connected = false
+    @name = nil
+    if not @yml['ftp']['users'].has_key?(args.strip!)
+      @cs.close
+      -1
+    end
+    @name = args
+    @cs.write "331 User name okay, need password.\r\n"
     0
   end
 
   def ftp_pass(c, args)
-    ftp_502 c, args
+    if @name.nil?
+      return ftp_503(c, args)
+    end
+    if not @yml['ftp']['users'][@name] == args.strip!
+      @cs.write "500 Illegal username\r\n"
+      @name = nil
+      return 0
+    end
+    @connected = true
+    @cs.write "230 User #{args} logged in.\r\n"
     0
   end
 
   def ftp_type(c, args)
+    if not @connected
+      return ftp_530(c, args)
+    end
     ftp_502 c, args
     0
   end
 
   def ftp_list(c, args)
+    if not @connected
+      return ftp_530(c, args)
+    end
     ftp_502 c, args
     0
   end
 
   def ftp_pasv(c, args)
+    if not @connected
+      return ftp_530(c, args)
+    end
+    ftp_502 c, args
+    0
+  end
+
+  def ftp_pwd(c, args)
+    if not @connected
+      return ftp_530(c, args)
+    end
+    ftp_502 c, args
+    0
+  end
+
+  def ftp_stor(c, args)
+    if not @connected
+      return ftp_530(c, args)
+    end
+    ftp_502 c, args
+    0
+  end
+
+  def ftp_retr(c, args)
+    if not @connected
+      return ftp_530(c, args)
+    end
     ftp_502 c, args
     0
   end
@@ -69,7 +130,7 @@ class FujiFtpClient
       ####
       begin
         func = method('ftp_' + line[/[\s]*([\w]*)[\s]+(.*)/, 1].downcase);
-        if func.call $1, $2 == -1
+        if func.call($1, $2) == -1
           break
         end
       rescue NameError => e
